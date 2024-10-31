b3337ac93c694226fad38823f2d409434e34529f
FernandoGmz2001 <fernandogmz2001@gmail.com>
2024-10-30 21:23:40 -0500

    feat: Add Left side header

--- app/home/components/HeaderLeft.tsx
+++ app/home/components/HeaderLeft.tsx
@@ -1,10 +1,17 @@
+'use client'
 import Breadcrumb from "./Breadcrumb";
+import InvitedUsers from "./InvitedUsers";
+import { FaRegShareSquare } from "react-icons/fa";
 
-export default function HeaderLeft({workspace}:{workspace: string}){
+export default function HeaderLeft({ workspace }: { workspace: string }) {
   return <>
     <div className="flex flex-col gap-2">
-      <Breadcrumb sections={['Workspace','Test','Board']}/>
+      <Breadcrumb sections={['Workspace', 'Test', 'Board']} />
       <h1 className="font-bold text-4xl">{workspace}</h1>
+      <div className="flex gap-4 items-center">
+        <InvitedUsers />
+        <div className="flex gap-4 items-center border px-4 py-2 rounded-xl border-gray-300"><FaRegShareSquare /><p className="font-medium">Compartir</p></div>
+      </div>
     </div>
   </>
 }
---
+++ app/home/components/InvitedUsers.tsx
@@ -0,0 +1,7 @@
+export default function InvitedUsers() {
+  return <>
+    <div className="rounded-full w-[30px] h-[30px]">
+      <img src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatarIcon"/>
+    </div>
+  </>
+}
